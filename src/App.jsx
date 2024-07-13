7f3d9c97fcbb080a94a356eaee33aae133b922a4
mubeejoy12 <83483923+mubeejoy12@users.noreply.github.com>
2024-07-13 18:13:15 +0100

    imported the navbar component  to the app component to make it universe

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
-
-import './App.css'
+import "./App.css";
+import Navbar from "./components/navbar/Navbar";
 
 function App() {
-  
-
   return (
     <>
-     
+      <Navbar />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
